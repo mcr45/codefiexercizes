7839d659e56ef14b871e4360146a2d9ed719e7b3
mcr45 <depr3604@protonmail.com>
2023-08-29 20:56:14 -0500

    first exercize,objects

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+   /* Set up a new JavaScript file dedicated to practicing with objects.
+    Construct an object representing a book, with properties such as title, author, publication year, and genre.
+    Implement functions to add, modify, and delete properties from this book object.
+    To ensure you've mastered the topic, access and display the book's properties.
+    Once satisfied, commit your JavaScript file to a GitHub repository. This will not only serve as a record but will also allow you to share your progress and findings with peers. */
+    
+    let book={
+    
+    title:'John',
+    author:'John, Bonjohn Jr',
+    "publication year":1956,
+    genre:'fantasy',
+    }
+    
+    for( let itm in book){
+    
+    console.log(itm)}
+    
+    
+    
