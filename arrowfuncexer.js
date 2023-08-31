ece05a25d7186eaaad5e4d62e1bb6d00bb77fc43
mcr45 <depr3604@protonmail.com>
2023-08-30 19:04:33 -0500

    arrow functions

---
+++ arrowfuncexer.js
@@ -0,0 +1,15 @@
+/* Construct a new JavaScript file for practicing the newly acquired concepts.
+Script a minimum of three arrow functions:
+    A function that accepts two arguments and returns their product.
+    A function that takes in a string and returns it in uppercase.
+    A function with a single parameter, calculating the square of the provided number.
+Validate the proper functioning of your functions.
+Upon completion, push your JavaScript file to a GitHub repository to maintain a record
+ and possibly share with others when needed. */
+
+
+ let first=(a,b)=>a*b;
+
+let second= (str)=> str.toUpperString();
+
+let third= (n)=>{return n**n }
