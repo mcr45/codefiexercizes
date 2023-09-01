9fa9f6673b09a322265dc2eff7e3d6e41099f232
mcr45 <depr3604@protonmail.com>
2023-08-31 20:02:27 -0500

    added 5 to 8 exercize

---
+++ aug31exer/exer5_8.js
@@ -0,0 +1,20 @@
+function reverto(n){
+    let seq=n.split('')
+    return seq.reverse().join('')
+}
+
+
+function calculateArea(x){
+
+    return (Math.PI)*(x**2)
+}
+
+function whattimeisit(){
+console.log(new Date)
+}
+
+
+function zeroOrTen(){
+
+return Math.floor(Math.random() *11)
+}
