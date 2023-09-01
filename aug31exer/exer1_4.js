e8ad641f3571760d396aeb68c0f938eaf9897a79
mcr45 <depr3604@protonmail.com>
2023-08-31 19:58:32 -0500

    first 4 exercize from august 31

---
+++ aug31exer/exer1_4.js
@@ -0,0 +1,23 @@
+
+
+
+
+function greet(name){
+    console.log(`Hello there ${name}`)
+}
+
+function moroby(a,b){
+    return a*b
+}
+
+const person={
+name:'OKay',
+age:90,
+gender:'M'
+}
+
+
+function reverto(n){
+let seq=n.split('')
+return seq.reverse().join('')
+}
